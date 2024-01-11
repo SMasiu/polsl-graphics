ca0efce6371ed31573e4802ec0e7e2aaf220a8ec
Szymon Maśko <47897497+SMasiu@users.noreply.github.com>
2024-01-11 11:07:15 +0100

    Add files via upload

---
+++ Zad6/Zad6/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Zad6
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Mat image = CvInvoke.Imread("C:\\Users\\szymo\\Desktop\\image.jpg", ImreadModes.Color);
+
+          
+            float[,] kernelData = {
+                { -1, -1, -1 },
+                { -1, 9, -1 },
+                { -1, -1, -1 }
+            };
+            Matrix<float> kernel = new Matrix<float>(kernelData);
+
+          
+            CvInvoke.Filter2D(image, image, kernel, new System.Drawing.Point(-1, -1));
+
+            CvInvoke.Imshow("Image", image);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
