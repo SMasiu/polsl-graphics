e9684a7ff15001ec718354c321baec3a4f5ca0bc
Szymon Maśko <47897497+SMasiu@users.noreply.github.com>
2024-01-11 11:06:06 +0100

    Add files via upload

---
+++ Zad5/Zad5/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Windows;
+
+namespace Zad5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Image<Bgr, byte> img = new Image<Bgr, byte>("C:\\Users\\szymo\\Desktop\\image.jpg");
+            Image<Gray, byte> img2 = img.Convert<Gray, byte>();
+            Image<Gray, Single> img_final = (img2.Sobel(1, 0, 5));
+
+            CvInvoke.Imshow("Image", img_final);
+            CvInvoke.Imshow("Image2", img2);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
